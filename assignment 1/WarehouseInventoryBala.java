6b65877a56cc711a6f9e34d1e3c6746fb8a88092
Shambhavi Singh <shammy6358@gmail.com>
2026-09-05 03:09:22 +0530

    Added Week 1 assignment problems

---
+++ assignment 1/WarehouseInventoryBala.java
@@ -0,0 +1,66 @@
+public class WarehouseInventoryBalancer {
+
+    public static void analyzeInventory(
+            int[] sectionA,
+            int[] sectionB) {
+
+        int totalA = 0;
+        int totalB = 0;
+
+        for (int i = 0; i < sectionA.length; i++) {
+            totalA = totalA + sectionA[i];
+            totalB = totalB + sectionB[i];
+        }
+
+        String status;
+
+        if (totalA == totalB) {
+            status = "Balanced";
+        } else {
+            status = "Not Balanced";
+        }
+
+        int highest = sectionA[0];
+        String highestSection = "Section A";
+        int highestIndex = 0;
+
+        for (int i = 0; i < sectionA.length; i++) {
+
+            if (sectionA[i] > highest) {
+                highest = sectionA[i];
+                highestSection = "Section A";
+                highestIndex = i;
+            }
+
+            if (sectionB[i] > highest) {
+                highest = sectionB[i];
+                highestSection = "Section B";
+                highestIndex = i;
+            }
+        }
+
+        System.out.println(
+                "Section A Total: " + totalA
+                + " | Section B Total: " + totalB
+                + " | Status: " + status
+        );
+
+        System.out.println(
+                "Highest Quantity: "
+                + highest
+                + " ("
+                + highestSection
+                + ", Item "
+                + (highestIndex + 1)
+                + ")"
+        );
+    }
+
+    public static void main(String[] args) {
+
+        int[] sectionA = {20, 15, 30};
+        int[] sectionB = {25, 10, 30};
+
+        analyzeInventory(sectionA, sectionB);
+    }
+}
